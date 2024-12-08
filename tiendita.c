7f34f557cda1f2b507c9171e9d6b358d214206e4
JICS <clavivan16@gmail.com>
2024-12-07 23:26:57 -0600

    Entrada de producto y venta

--- tiendita.c
+++ tiendita.c
@@ -6,6 +6,13 @@
 #define PAUSA system("pause")
 
 /*****************************************************************************
+* Los tipos de datos van en mayusculas 
+* Las variables globales la primera letra en mayusculas y el resto en 
+* minusculas
+* Las variables locales y los parametros en minusculas
+*****************************************************************************/
+
+/*****************************************************************************
  Definicion de estructuras
 *****************************************************************************/
 typedef struct struct_producto PRODUCTO;
@@ -68,10 +75,12 @@ LST_VENTA Ventas;
 /*** producto ***/
 PRODUCTO *nuevo_producto(void);
 void agregar_producto(LST_PRODUCTO*,PRODUCTO);
+void entrada_producto();
 
 /*** producto venta ***/
 PRODUCTO_VENTA *nuevo_producto_venta(void);
 void agregar_producto_venta(LST_PRODUCTO_VENTA*,PRODUCTO_VENTA);
+LST_PRODUCTO_VENTA entrada_producto_venta(int num_venta);
 
 /*** venta ***/
 VENTA *nueva_venta(void);
@@ -98,8 +107,28 @@ void agregar_producto(LST_PRODUCTO *lista, PRODUCTO producto) {
 		nuevo->siguiente=NULL;
 		lista->fin=nuevo;
 	}
+    else{
+        lista->inicio=nuevo;
+        lista->fin=nuevo;      
+    }
 }
 
+void entrada_producto(){
+    PRODUCTO producto;
+    
+    printf("ID: ");
+    scanf(" %d",&producto.id_producto);
+    printf("Nombre del producto: ");
+    scanf(" %50[^\n]",producto.nombre_producto);
+    printf("Precio de compra: ");
+    scanf(" %f",producto.precio_compra);
+    printf("Unidades en existencia: ");
+    scanf(" %d",producto.existencia);
+    printf("Precio de venta: ");
+    scanf(" %f",producto.precio_venta);
+    
+    agregar_producto(&Productos,producto);
+}
 /*****************************************************************************
  Funciones de producto venta
 *****************************************************************************/
@@ -115,6 +144,37 @@ void agregar_producto_venta(LST_PRODUCTO_VENTA *lista, PRODUCTO_VENTA producto_v
 		nuevo->siguiente=NULL;
 		lista->fin=nuevo;
 	}
+    else{
+        lista->inicio=nuevo;
+        lista->fin=nuevo;      
+    }    
+}
+
+LST_PRODUCTO_VENTA entrada_producto_venta(int num_venta){
+    PRODUCTO_VENTA producto_venta;
+    LST_PRODUCTO_VENTA lista_producto_venta;
+    char buffer[50];
+    
+    lista_producto_venta.inicio=NULL;
+    lista_producto_venta.fin=NULL;
+    
+    producto_venta.num_venta=num_venta;
+    do{                    
+        printf("Numero de producto: ");
+        scanf(" %d",&producto_venta.num_producto);
+        printf("Cantidad: ");
+        scanf(" %d",&producto_venta.cantidad);
+        
+        producto_venta.producto=NULL;
+        producto_venta.siguiente=NULL;
+        
+        agregar_producto_venta(&lista_producto_venta,producto_venta);
+        
+        printf("Escriba fin para terminar, cualquier otra para agregar un "
+                "producto adicional\n");
+        scanf(" %s",buffer);
+    }while(strcmp("fin",buffer)!=0);
+    return lista_producto_venta;
 }
 
 /*****************************************************************************
@@ -132,6 +192,24 @@ void agregar_venta(LST_VENTA *lista, VENTA venta) {
 		nuevo->siguiente=NULL;
 		lista->fin=nuevo;
 	}
+    else{
+        lista->inicio=nuevo;
+        lista->fin=nuevo;      
+    }    
+}
+
+void entrada_venta(){
+    VENTA venta;
+    
+    printf("Numero de venta: ");
+    scanf(" %d",&venta.num_venta);
+    printf("Fecha de venta: ");
+    scanf(" %d",&venta.fecha);
+    printf("Hora de venta: ");
+    scanf(" %d",&venta.hora);
+    
+    venta.lista_productos=entrada_producto_venta(venta.num_venta);
+    agregar_venta(&Ventas,venta);
 }
 
 /*****************************************************************************
@@ -190,6 +268,7 @@ void menu_ventas() {
 		scanf(" %d",&opcion);
 		switch (opcion) {
 			case 1:
+                entrada_venta();
 				break;
 			case 2:
 				break;
@@ -220,10 +299,9 @@ int main() {
 		LIMPIAR_PANTALLA;
 		printf("Menu principal");
 		printf("\n1. Menu Productos");
-		printf("\n2. Menu Clientes");
-		printf("\n3. Menu Ventas");
-		printf("\n4. Guardar todos los datos");
-		printf("\n5. Salir");
+		printf("\n2. Menu Ventas");
+		printf("\n3. Guardar todos los datos");
+		printf("\n4. Salir");
 		printf("\nOpcion: ");
 		scanf("%d", &opcion);
 
